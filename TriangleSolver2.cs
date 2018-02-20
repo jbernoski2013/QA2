1f940ea552a2a9ed009bc577c2245c16b7dcaaba
Julia Bernoski <Jbernoski2013@conestogac.on.ca>
2018-02-20 11:54:47 -0500

    Completed Class

---
+++ TriangleSolver2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070A2
+{
+    class TriangleSolver
+    {
+       
+        public  void analyze(decimal a, decimal b, decimal c)
+        {
+            if(a + b > c && a + c > b && b + c > a)
+            {
+                Console.WriteLine("Forms a triangle");
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Triangle is an equilateral");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("Traingle is an isosceles");
+                }
+                else if (a != b && b != c && c != a)
+                {
+                    Console.WriteLine("Triangle is Scalene");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Does not form a triangle");
+            }
+           
+        }
+
+        
+    }
+}
